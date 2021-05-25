fb47ee34d9464cc60cc802b7ecc72ee0dd6f7087
PavanSriram <pavangud9@gmail.com>
2021-05-25 12:11:49 +0530

    original index.js in backed

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-
-
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -8,15 +5,4 @@ const PORT = 4000;
 app.use(cors());
 app.listen(PORT, function() {
   console.log("Server is running on Port: " + PORT);
-=======
-
-
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const PORT = 4000;
-app.use(cors());
-app.listen(PORT, function() {
-  console.log("Server is running on Port: " + PORT);
->>>>>>> 19d5a9c8cb9dd9a56efc03dd007d59d8c00c494a
 });
